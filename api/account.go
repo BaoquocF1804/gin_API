d9475c2ae3ad9c3b49ac10b08b4164ebe02fb05a
BaoquocF1804 <s2chungbeov2@gmail.com>
2023-06-19 11:56:43 +0700

    complete get_post_getlist

--- api/account.go
+++ api/account.go
@@ -58,24 +58,23 @@ func (server *Server) getAccount(ctx *gin.Context) {
 
 type listAccountRequest struct {
 	PageID   int32 `form:"page_id" binding:"required"`
-	PageSize int32 `form:"page_id" binding:"required"`
+	PageSize int32 `form:"page_size" binding:"required"`
 }
 
 func (server *Server) listAccount(ctx *gin.Context) {
 	var req listAccountRequest
-	if err := ctx.ShouldBindUri(&req); err != nil {
+	if err := ctx.ShouldBindQuery(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 	}
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+	account, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 
-	////account, err := server.store.GetAccount(ctx, req.ID)
-	//if err != nil {
-	//	if err == sql.ErrNoRows {
-	//		ctx.JSON(http.StatusNotFound, errorResponse(err))
-	//		return
-	//	}
-	//	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-	//	return
-	//}
-	//
-	//ctx.JSON(http.StatusOK, account)
+	ctx.JSON(http.StatusOK, account)
 }
